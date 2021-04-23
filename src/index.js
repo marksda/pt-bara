b09faf10a6400f5d79743140203a07d2a833c998
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-04-24 06:01:30 +0700

    cso

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import store from "./store/index";
+import { BrowserRouter as Router } from "react-router-dom";
+import RoutePage from './RoutePage';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <RoutePage />
+      </Router>    
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
