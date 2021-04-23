0173630c7df3f42997f433ae16bc39ff57502602
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-04-24 06:33:22 +0700

    cso

---
+++ src/constants/master-types.js
@@ -0,0 +1,3 @@
+export const CREDENTIAL = "credential_profile";
+export const HEADER_AUTHORIZATION = "header_authorization";
+export const USER_PROFILE = "user_profile";
--- src/reducers/credential.js
+++ src/reducers/credential.js
@@ -1,4 +1,6 @@
 import CryptoJS from 'crypto-js';
+import { CREDENTIAL_LOADED, CREDENTIAL_RESET, USER_LOADED } from "../constants/action-types";
+import { CREDENTIAL, HEADER_AUTHORIZATION, USER_PROFILE } from "../constants/master-types";
 
 const initialState = {
     user_profile: {
@@ -40,6 +42,33 @@ const loadLocalProfileStorage = () => {
 
 export default function(state = initialState, action) {
     switch (action.type) {
+        case USER_LOADED:
+            let userProfile = {
+                accountId: action.payload.nip,
+                accountRealName: action.payload.nama
+            }
+            let ciphertextprofile = CryptoJS.AES.encrypt(JSON.stringify(userProfile), '6MjSltMV8cadJWcKh_nR4Ds7vc$!ftDw');
+            window.localStorage.setItem('{8e9cca41-2ddf-e5c8-5af2-6dd2f36ceae4}', ciphertextprofile);
+            return {
+                ...state,
+                [USER_PROFILE]: userProfile
+            };
+        case CREDENTIAL_LOADED:
+            let ciphertexttoken = CryptoJS.AES.encrypt(JSON.stringify(action.payload), '6MjSltMV8cadJWcKh_nR4Ds7vc$!ftDw');
+            window.localStorage.setItem('{ef535818-9c4d-1b92-9eea-43ad6d745c9a}', ciphertexttoken);
+            return {
+                ...state,
+                [CREDENTIAL]: action.payload,
+                [HEADER_AUTHORIZATION]: 'Bearer ' + action.payload.token
+            };
+        case CREDENTIAL_RESET:
+            window.localStorage.clear();
+            return{
+                ...state,
+                [USER_PROFILE]: initialState.userProfile,
+                [CREDENTIAL]: initialState.credential_profile,
+                [HEADER_AUTHORIZATION]: initialState.header_authorization
+            }
         default:
             let cr = loadLocalCredentialStorage();
             let pfl = loadLocalProfileStorage();
