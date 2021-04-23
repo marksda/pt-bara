64ecf87ed8f6dddf23c05ee5f79d384277a36e60
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-04-24 06:08:08 +0700

    cso

--- src/RoutePage.js
+++ src/RoutePage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+import Home from './components/pages/Home';
 
 class RoutePage extends React.Component {
-
+    render() {
+        <Switch>
+            <Route exact path="/" component={Home} />
+        </Switch>
+    }
 }
 
 export default RoutePage;
