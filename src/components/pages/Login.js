ca6ed68138d7df58411ec43b89b5f1cfcede5cca
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-04-26 17:02:56 +0700

    cso

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -51,12 +51,54 @@ class Login extends React.Component {
 		super(props);
 
 		this.state = {
-			isProgress: false
+			isProgress: false,
+            isDisabled: false,
+            isErrorUserName: false
 		};
+
+        this.errorUserNameMessage = null;
+        this.userName = '';
 	}
+
+    handleBtnShowStep2Click = (e) => {
+        e.preventDefault();
+        if(this.verifikasiUserName()) {
+            this.validateUsername();
+        }
+    }
+
+    handleKeyUpUsername = (e) => {
+        if (e.key === 'Enter') {
+            if(this.verifikasiUserName()) {
+                this.validateUsername();
+            }
+        }
+    }
+
+    onChangeUsername = (e) => {
+        this.userName = e.target.value;
+    }
+
+    validateUsername = () => {
+        console.log('cek remote user name');
+    }
+
+    verifikasiUserName = () => {
+        if(this.userName.length === 0) {
+            this.errorUserNameMessage = "User name harus diisi";
+            this.setState({isErrorUserName: true});
+            return false;
+        }
+        else {
+            this.errorUserNameMessage = '';
+            this.setState({isErrorUserName: false, isProgress: true, isDisabled: true});
+            return true;
+        }
+    }
+
     render() {
-        const { classes, authorizationNotify } = this.props;
-        const { isProgress } = this.state;
+        const { authorizationNotify, classes } = this.props;
+        const { isDisabled, isErrorUserName, isProgress } = this.state;
         let page = null;
 
         if(authorizationNotify === 'authorization') {
@@ -69,30 +111,45 @@ class Login extends React.Component {
                     isProgress === true ? <LinearProgress classes={{root: classes.progresBorderIndikator}}/> : null
                 }
                 <div className="container-login-body">
-                <Typography component="div" className={classes.verticalSpacing32}>
-                    <Box fontWeight="500" fontSize="h6.fontSize" textAlign="center" m={1}>
-                        Sign in
-                    </Box>
-                    <Box textAlign="center" m={1}>
-                        untuk lanjut ke sistem informasi keuangan
-                    </Box>
-                </Typography>
-                <TextField 
-                    classes={{ root: classes.verticalSpacing48 }} 
-                    id="outlined-basic" 
-                    label="User name" 
-                    variant="outlined" 
-                    fullWidth={true}
-                    autoFocus={true}
-                />
-                <Typography component="div" className={classes.verticalSpacing24}>
-                    <Box textAlign="left" fontWeight="400" fontSize={12} m={0}>
-                    Gunakan mode private untuk sign in, jika memakai komputer atau gadget orang lain.
-                    </Box>
-                </Typography>
-                <Button variant="contained" color="primary" size="large" style={{width: 100, float: 'right'}}>                
-                Next
-                </Button>
+                    <Typography component="div" className={classes.verticalSpacing32}>
+                        <Box fontWeight="500" fontSize="h6.fontSize" textAlign="center" m={1}>
+                            Sign in
+                        </Box>
+                        <Box textAlign="center" m={1}>
+                            untuk lanjut ke sistem informasi keuangan
+                        </Box>
+                    </Typography>
+                    <section id="step1" className="slide-in from-left show">
+                        <TextField
+                            autoFocus={true} 
+                            classes={{ root: classes.verticalSpacing48 }} 
+                            disabled={isDisabled}
+                            error={isErrorUserName} 
+                            fullWidth={true}
+                            id="outlined-basic" 
+                            label="User name"
+                            onChange={this.onChangeUsername}
+                            onKeyUp={this.handleKeyUpUsername}
+                            variant="outlined"
+                            required={true}
+                            helperText={this.errorUserNameMessage}
+                        />
+                        <Typography component="div" className={classes.verticalSpacing24}>
+                            <Box textAlign="left" fontWeight="400" fontSize={12} m={0}>
+                            Gunakan mode private untuk sign in, jika memakai komputer atau gadget orang lain.
+                            </Box>
+                        </Typography>
+                        <Button 
+                            variant="contained" 
+                            color="primary" 
+                            size="large" 
+                            disabled={isDisabled}
+                            style={{width: 100, float: 'right'}}
+                            onClick={this.handleBtnShowStep2Click}
+                        >                
+                        Next
+                        </Button>
+                    </section>
                 </div>
             </Paper>;
             
