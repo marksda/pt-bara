d74594c4188d65f2e85c8164dcd50968a471ec1b
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-04 08:32:12 +0700

    cso

--- src/RoutePage.js
+++ src/RoutePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import Login from './components/pages/Login';
+import Main from './components/pages/main';
 
 class RoutePage extends React.Component {
     render() {
         return (
             <Switch>
                 <Route exact path="/" component={Login} />
+                <Route exact path="/main" component={Login} />
             </Switch>
         );
     }
---
+++ src/components/pages/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+class Login extends React.Component {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(Login));
