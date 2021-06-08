51b185f21642e374130b51c1531a15f551996594
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-08 16:47:52 +0700

    cso

--- src/components/forms/Form-Pengajuan-Proyek.js
+++ src/components/forms/Form-Pengajuan-Proyek.js
@@ -4,7 +4,23 @@ class FormPengajuanProyek extends React.Component {
     render() {
         let page =
         <table className="table-container-pengajuan-baru">
-            
+            <tbody>
+                <tr>
+                    <td style={{paddingBottom: 16}}>
+						<div className="table-container-cel-pengajuan-baru">
+							<Text strong className="item-label">Tgl. usulan</Text>
+							<DatePicker 
+								format="DD-MM-YYYY"
+								defaultValue={moment(detailReimburse.tglusul)}
+								onChange={
+									(date, dateString) => 
+									this.handleChangeTanggal(date, dateString, "tglusul")
+								}
+							/>
+						</div>
+					</td>
+                </tr>
+            </tbody>
         </table>;
 
         return(page);
