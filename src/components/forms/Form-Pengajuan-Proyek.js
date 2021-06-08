6b3ff1f67e392480e72e7af565ffb533a67e4b44
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-08 16:25:43 +0700

    cso

---
+++ src/components/forms/Form-Pengajuan-Proyek.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FormPengajuanProyek extends React.Component {
+    render() {
+        let page =
+        <table className="table-container-pengajuan-baru">
+            
+        </table>;
+
+        return(page);
+    }
+}
+
+export default FormPengajuanProyek;
--- src/components/pages/Pengajuan-Baru.js
+++ src/components/pages/Pengajuan-Baru.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Divider, Radio } from 'antd';
 
+import FormPengajuanProyek from '../forms/Form-Pengajuan-Proyek';
+
 class PengajuanBaru extends React.Component {
     constructor(props) {
 		super(props);
@@ -18,13 +20,14 @@ class PengajuanBaru extends React.Component {
         const { itemTabSelected } = this.state;
         let page =
         <>
-        <div className="content-master">
-            <Radio.Group value={itemTabSelected} onChange={this.handleChangeItemTab}>
-                <Radio.Button value="Proyek">Proyek</Radio.Button>
-                <Radio.Button value="Non_Proyek">Non-Proyek</Radio.Button>
-            </Radio.Group>  
-        </div>                  
-        <Divider style={{borderTop: '1px solid rgba(17, 123, 236, 0.54)'}}/>
+            <div className="content-master">
+                <Radio.Group value={itemTabSelected} onChange={this.handleChangeItemTab}>
+                    <Radio.Button value="Proyek">Proyek</Radio.Button>
+                    <Radio.Button value="Non_Proyek">Non-Proyek</Radio.Button>
+                </Radio.Group>  
+            </div>                  
+            <Divider style={{borderTop: '1px solid rgba(17, 123, 236, 0.54)'}}/>
+            <FormPengajuanProyek />
         </>;
         return(page);
     }
