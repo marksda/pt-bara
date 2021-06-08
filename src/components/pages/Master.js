4a11aca413f50892bf89fef690b0a8681ea4d3a0
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-08 07:21:27 +0700

    cso

--- src/components/pages/Master.js
+++ src/components/pages/Master.js
@@ -3,9 +3,32 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
 class Master extends React.Component {
-    render() {
-        let page = <div>Master</div>;
+    constructor(props) {
+		super(props);
 
+        this.state = {
+            itemTabSelected: 0
+        }
+    }
+
+    handleChangeItemTab = (e, newValue) => {
+        this.setState({itemTabSelected: })
+    }
+
+    render() {
+        const { itemTabSelected } = this.state;
+        let page =
+        <Tabs
+            value={itemTabSelected}
+            onChange={this.handleChangeItemTab}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+        >
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+        </Tabs>;
         return(page);
     }
 }
