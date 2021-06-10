52bce3ab630bda0a069031ebad1fde47da043430
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-10 14:17:15 +0700

    cso

---
+++ src/components/forms/Form-Add-Customer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+
+class FormAddCustomer extends Component {
+    render() {
+        
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FormAddCustomer);
