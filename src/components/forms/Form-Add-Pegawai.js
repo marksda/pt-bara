41c959e744cfe35620d932eef0bf88676a2179b3
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-12 16:40:13 +0700

    cso

--- src/components/forms/Form-Add-Pegawai.js
+++ src/components/forms/Form-Add-Pegawai.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import { Button, Form, Input, Modal, Select } from 'antd';
+import { Button, Form, Input, Modal, Select, Upload } from 'antd';
 import { connect } from "react-redux";
 import { getPegawai } from "../../actions/master-action";
+
+import { DeleteOutlined, LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+
 
 const mapStateToProps = store => {
     return {      
@@ -34,8 +37,11 @@ class FormAddPegawai extends Component {
 		super(props);
 		this.state = {
 			disabledInput: false,
+            imageUrl: null,
+            loading: false
 		};
 
+        this.formData = null;
 		this.formRef = React.createRef();
 		this.itemPegawai = {};
 	}
@@ -47,6 +53,52 @@ class FormAddPegawai extends Component {
             this.itemPegawai = {...data};
         }
     }
+
+    beforeUpload = (file) => {
+		let oFReader = new FileReader();
+		oFReader.readAsDataURL(file);
+		let img = document.getElementById("img-preview-pegawai");
+		oFReader.onload = function(oFREvent) {
+	    	img.src = oFREvent.target.result;
+	    	img.onload = function () {
+				if(this.width >= this.height) {
+					alert("ratio image harus portrait");
+				}
+				else {
+					let elt = document.querySelectorAll("[data-id='foto_pegawai']");
+					elt[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].style.display = "none";
+					let imgPegawaiPreview = elt[0].childNodes[1].childNodes[0].childNodes[0].childNodes[1];
+					let lebar = 0;
+					let tinggi = 0;
+					if(this.width>=100) {
+						lebar = this.width/10;
+						tinggi = this.height/10;
+					}
+					else {
+						lebar = 218;
+						tinggi = 327;
+					}					
+					imgPegawaiPreview.style.width = lebar.toString()+'px';
+					imgPegawaiPreview.style.height = tinggi.toString()+'px';
+					imgPegawaiPreview.style.display = "block";
+				}
+			};
+	    };
+
+		if(this.formData === null) {
+	    	this.formData = new FormData();
+	    }
+	    this.formData.append('file', file);
+
+	    return false;	
+	}
+
+    handleBtnDeleteImgPegawai = (e) => {
+		let elt = document.querySelectorAll("[data-id='foto_pegawai']");
+		elt[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0].style.display = "block";
+		elt[0].childNodes[1].childNodes[0].childNodes[0].childNodes[1].style.display = "none";
+		this.formData.delete('file');
+	}
 
     handleChangeNilaiText = (e) => {
         const { mode } = this.props;
@@ -76,16 +128,28 @@ class FormAddPegawai extends Component {
 	}
 
     handleChangeStatus = (value) => {
-		this.itemCustomer.status = Number(value);			
+		this.itemPegawai.status = value;			
 	}
 
     handleOnFinish = (value) => {
 		const { mode } = this.props;
 		this.setState({disabledInput: true});
+        if(this.formData === null) {
+	    	this.formData = new FormData();
+	    }
+
+        this.formData.append('nama', this.itemPegawai.nama);
+        this.formData.append('no_handphone', this.itemPegawai.no_handphone);
+        this.formData.append('alamat', this.itemPegawai.alamat);
+        this.formData.append('email', this.itemPegawai.email);
+
 		if(mode === 'edit') {
+            this.formData.append('niplama', this.itemPegawai.nip);
+            this.formData.append('nip', this.itemPegawai.nipbaru);
             this.updatePegawai();
         }
-        else {
+        else {          
+            this.formData.append('nip', this.itemPegawai.nip);          
 			this.savePegawai();
         }
 	}
@@ -101,31 +165,35 @@ class FormAddPegawai extends Component {
     }
 
     savePegawai = () => {
-		const { 
-			filterPegawai, headerAuthorization, paginationPegawai, restfulServer, urutPegawai, handleClose, handleToggleOpenProgressDialog
-		} = this.props;
-	    let self = this;
+        console.log(this.formData);
+		// const { 
+		// 	filterPegawai, headerAuthorization, paginationPegawai, restfulServer, urutPegawai, handleClose, handleToggleOpenProgressDialog
+		// } = this.props;
+	    // let self = this;
         
-	    handleToggleOpenProgressDialog();
+	    // handleToggleOpenProgressDialog();
 
-	    axios({
-            method: 'put',
-            url: `${restfulServer}/master/pegawai`,
-            headers: {...headerAuthorization},
-            data: this.itemPegawai
-        })
-	    .then((r) => {  
-	    	if(r.data.status === 200) {        
-				self.loadPegawai(filterPegawai, paginationPegawai, urutPegawai);
-	    	} 
-	    	self.handleReset();
-            self.setState({disabledInput: false});
-            // handleClose();
-            handleToggleOpenProgressDialog();
-	    })
-	    .catch((r) => {
-	    	self.setState({disabledInput: true});
-	    });
+	    // axios({
+        //     method: 'put',
+        //     url: `${restfulServer}/master/pegawai`,
+        //     headers: {...headerAuthorization},
+        //     data: this.formData
+        // })
+	    // .then((r) => {  
+	    // 	if(r.data.status === 200) {        
+        //         self.formData = null;
+        //         self.itemPegawai = {};
+		// 		self.loadPegawai(filterPegawai, paginationPegawai, urutPegawai);
+	    // 	} 
+	    // 	self.handleReset();
+        //     self.setState({disabledInput: false});
+        //     handleToggleOpenProgressDialog();
+	    // })
+	    // .catch((r) => {
+        //     self.formData = null;
+        //     self.itemPegawai = {};
+	    // 	self.setState({disabledInput: true});
+	    // });
 	}    
 
     updatePegawai = () => {
@@ -137,10 +205,12 @@ class FormAddPegawai extends Component {
             method: 'post',
             url: `${restfulServer}/master/pegawai`,
             headers: {...headerAuthorization},
-            data: this.itemPegawai
+            data: this.formData
         })
         .then((r) => {         
             if(r.data.status === 200) {
+                self.formData = null;
+                self.itemPegawai = {};
                 self.loadPegawai(
                     filterPegawai,
                     paginationPegawai,
@@ -151,13 +221,21 @@ class FormAddPegawai extends Component {
             handleClose();
         })
         .catch((r) => {         
+            self.formData = null;
+            self.itemPegawai = {};
             self.setState({disabledInput: false});
         });        
     }
 
     render() {
         const { data, handleClose, mode, visible } = this.props;
-		const { disabledInput } = this.state;
+		const { disabledInput, imageUrl, loading } = this.state;
+        const uploadButton = (
+            <div>
+                {loading ? <LoadingOutlined /> : <PlusOutlined />}
+                <div style={{ marginTop: 8 }}>Upload</div>
+            </div>
+        );
 
 		let page = null;
 
@@ -190,12 +268,12 @@ class FormAddPegawai extends Component {
 	                label="Nip"
                     name="nip"
                     rules={[{required: true, message: 'Nip pegawai harus diisi'}]}
-                    style={{ width: 120 }}
                 >
                     <Input 
                         data-jenis="nip"
                         disabled={disabledInput}
                         onChange={this.handleChangeNilaiText}
+                        style={{ width: 120 }}
                     />
                 </Form.Item>
                 <Form.Item
@@ -212,7 +290,6 @@ class FormAddPegawai extends Component {
                 <Form.Item
 	                label="Alamat"
                     name="alamat"
-                    rules={[{required: true, message: 'Alamat pegawai harus diisi'}]}
                 >
                     <Input 
                         data-jenis="alamat"
@@ -223,7 +300,6 @@ class FormAddPegawai extends Component {
                 <Form.Item
 	                label="Telepone"
                     name="no_handphone"
-                    rules={[{required: true, message: 'Alamat pegawai harus diisi'}]}
                 >
                     <Input 
                         data-jenis="no_handphone"
@@ -234,7 +310,6 @@ class FormAddPegawai extends Component {
                 <Form.Item
 	                label="E-mail"
                     name="email"
-                    rules={[{required: true, message: 'E-mail pegawai harus diisi'}]}
                 >
                     <Input 
                         data-jenis="email"
@@ -254,6 +329,28 @@ class FormAddPegawai extends Component {
                         <Select.Option  value={true}>Aktif</Select.Option>
                         <Select.Option  value={false}>Non Aktif</Select.Option>
                     </Select>
+                </Form.Item>
+                <Form.Item
+                    label="Foto"
+                    name="foto"
+                    style={{marginTop: 16}}	                    
+                    data-id="foto_pegawai"
+                >
+                    <Upload
+                        name="foto_pegawai"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        beforeUpload={this.beforeUpload}
+                    >
+                        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                    </Upload>
+                    <div className="container-preview-img-pegawai">
+                        <img id="img-preview-pegawai" alt="foto" className="preview-img-pegawai"/>
+                        <div className="preview-img-pegawai-middle">
+                            <DeleteOutlined style={{ fontSize: '18px' }} onClick={this.handleBtnDeleteImgPegawai}/>                              
+                        </div>
+                    </div>
                 </Form.Item>
                 <Form.Item {...tailLayout}>
                     <Button 
