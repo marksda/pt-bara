a99d4e71f015d190b16be11181048500fe8544e7
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-06-14 14:19:12 +0700

    CSO

--- src/actions/master-action.js
+++ src/actions/master-action.js
@@ -1,5 +1,46 @@
-import { CREDENTIAL_RESET, FILTER_CUSTOMER_LOADED, LIST_CUSTOMER_LOADED, LIST_CUSTOMER_RESET, MENU_LOADED, PAGINATION_CUSTOMER_LOADED, UNAUTHORIZATION_RISE, URUT_CUSTOMER_LOADED, FILTER_JABATAN_LOADED, LIST_JABATAN_LOADED, LIST_JABATAN_RESET, PAGINATION_JABATAN_LOADED, URUT_JABATAN_LOADED, FILTER_BENTUK_USAHA_LOADED, LIST_BENTUK_USAHA_LOADED, LIST_BENTUK_USAHA_RESET, PAGINATION_BENTUK_USAHA_LOADED, URUT_BENTUK_USAHA_LOADED, FILTER_PEGAWAI_LOADED, LIST_PEGAWAI_LOADED, LIST_PEGAWAI_RESET, PAGINATION_PEGAWAI_LOADED, URUT_PEGAWAI_LOADED } from "../constants/action-types";
+import { CREDENTIAL_RESET, FILTER_CUSTOMER_LOADED, LIST_CUSTOMER_LOADED, LIST_CUSTOMER_RESET, MENU_LOADED, PAGINATION_CUSTOMER_LOADED, UNAUTHORIZATION_RISE, URUT_CUSTOMER_LOADED, FILTER_JABATAN_LOADED, LIST_JABATAN_LOADED, LIST_JABATAN_RESET, PAGINATION_JABATAN_LOADED, URUT_JABATAN_LOADED, FILTER_BENTUK_USAHA_LOADED, LIST_BENTUK_USAHA_LOADED, LIST_BENTUK_USAHA_RESET, PAGINATION_BENTUK_USAHA_LOADED, URUT_BENTUK_USAHA_LOADED, FILTER_PEGAWAI_LOADED, LIST_PEGAWAI_LOADED, LIST_PEGAWAI_RESET, PAGINATION_PEGAWAI_LOADED, URUT_PEGAWAI_LOADED, FILTER_STRUKTUR_ORGANISASI_LOADED, LIST_STRUKTUR_ORGANISASI_LOADED, LIST_STRUKTUR_ORGANISASI_RESET, PAGINATION_STRUKTUR_ORGANISASI_LOADED, URUT_STRUKTUR_ORGANISASI_LOADED } from "../constants/action-types";
 
+
+export const getStrukturOrganisasi = (url='', authorization) => {
+    return (dispatch) => {
+        return fetch(url, {
+            method: 'GET', 
+            headers: authorization
+        })
+        .then(            
+            (response) => {
+                return response.json();
+            }
+        )
+        .then(
+            json => {          
+                if(json.status === 200 ) {
+                    dispatch({ type: LIST_STRUKTUR_ORGANISASI_LOADED, payload: json.keterangan });
+                }
+                else {
+                    dispatch({ type: LIST_STRUKTUR_ORGANISASI_LOADED, payload: null });                    
+                }
+            }
+        )
+        .catch((e) => {
+            dispatch({type: LIST_STRUKTUR_ORGANISASI_RESET}); 
+            dispatch({type: CREDENTIAL_RESET});
+            dispatch({type: UNAUTHORIZATION_RISE});
+        });
+    };
+}
+
+export const setFilterStrukturOrganisasi = (value) => {
+    return({type: FILTER_STRUKTUR_ORGANISASI_LOADED, payload: value});
+}
+
+export const setPaginationStrukturOrganisasi = (value) => {
+    return({type: PAGINATION_STRUKTUR_ORGANISASI_LOADED, payload: value});
+}
+
+export const setUrutStrukturOrganisasi = (value) => {
+    return({type: URUT_STRUKTUR_ORGANISASI_LOADED, payload: value});
+}
 
 export const getPegawai = (url='', authorization) => {
     return (dispatch) => {
