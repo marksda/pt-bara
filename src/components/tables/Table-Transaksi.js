258191cab97033b3eeb8e56a5bf8a85170e226fb
Syaiful Dwi Anwar <vlack123@gmail.com>
2021-07-24 13:05:37 +0700

    cso

--- src/components/tables/Table-Transaksi.js
+++ src/components/tables/Table-Transaksi.js
@@ -402,6 +402,12 @@ class TableTransaksi extends React.Component {
         setItemMenuSelected('Transaksi Baru');
     }
 
+    handleBtnDelete = (e) => {
+        const { listTransaksi } = this.props;
+        this.itemTransaksi = {..._.find(listTransaksi.data, function(o) { return o.id === e.currentTarget.dataset.id; })};
+        this.setState({openConfirmasiHapusTransaksi: true});
+    }
+
     handleChangeRowsPerPage = (event) => {
         const { filterTransaksi, setPaginationTransaksi, urutTransaksi } = this.props;
         let tmpPagination = {
@@ -432,6 +438,24 @@ class TableTransaksi extends React.Component {
         }
     }
 
+    handleEditTransaksiBaru = (e) => {
+        const { headerAuthorization, setItemMenuSelected, setItemTransaksiSelected, setModeTransaksiBaru, restfulServer } = this.props;
+        setItemTransaksiSelected(`${restfulServer}/master/detailtransaksi?id=${e.currentTarget.dataset.id}`, headerAuthorization);  
+        setModeTransaksiBaru('edit');
+        setItemMenuSelected('Transaksi Baru');
+    }
+
+    handleRequestSort = (event, property) => {   
+        const { filterTransaksi, paginationTransaksi, setUrutTransaksi, urutTransaksi } = this.props;
+        let isAsc = urutTransaksi.field === property && urutTransaksi.order === 'asc';
+        let tmpUrut = {
+            field: property,
+            order: isAsc ? 'desc' : 'asc'
+        };
+        setUrutTransaksi(tmpUrut);
+        this.loadTransaksi(filterTransaksi, paginationTransaksi, tmpUrut);
+    }
+
     loadTransaksi = (filter, pagination, urut) => {
         const { getTransaksi, headerAuthorization, restfulServer } = this.props; 
         let url;
@@ -458,7 +482,73 @@ class TableTransaksi extends React.Component {
                 isProgress={isProgress}
             />
             <TableContainer className={classes.tableWrapper}>
-
+                <Table aria-labelledby="table-transaksi">
+                    <EnhancedTableHead 
+                        classes={classes}
+                        order={urutTransaksi.order}
+                        orderBy={urutTransaksi.field}
+                        onRequestSort={this.handleRequestSort}
+                    />
+                    <TableBody>
+                    {
+                    	listTransaksi !== null ? listTransaksi.data.map((row, index) => {
+                    		return(
+                    			<TableRow 
+	                                hover
+	                                tabIndex={-1}
+	                                key={row.no_job}      
+	                            >
+	                            	<TableCell 
+	                                    align={'right'}
+	                                    style={{minWidth: 40, verticalAlign: 'top'}}
+	                                >
+	                                    {(paginationTransaksi.current-1)*paginationTransaksi.pageSize+index+1}.
+	                                </TableCell>
+                                    <TableCell 
+	                                    align={'left'}
+                                        style={{width: 150, verticalAlign: 'top'}}
+	                                >
+	                                    { 
+                                            this.flipDate(row.tanggal)                                      
+                                        }
+	                                </TableCell>
+	                                <TableCell 
+	                                    align={'left'}
+                                        style={{width: 200, verticalAlign: 'top'}}
+	                                >
+	                                    { row.is_proyek === true ? 'Proyek' : 'Non Proyek' }
+	                                </TableCell>
+                                    <TableCell 
+	                                    align={'left'}
+                                        style={{verticalAlign: 'top'}}
+	                                >
+	                                    { row.transaksi }
+	                                </TableCell>
+	                                <TableCell 
+                                        style={{width: 100, verticalAlign: 'top', cursor: isProgress===false?'pointer':'default'}}
+                                        align={'center'}
+                                    >
+                                        <PlusOutlined 
+                                            style={{ fontSize: '18px', marginRight: 4}} 
+                                            onClick={isProgress===false?this.handleAddTransaksiBaru:null}
+                                        />
+                                        <EditOutlined 
+                                            style={{ fontSize: '18px', marginRight: 4}} 
+                                            data-id={row.id} 
+                                            onClick={isProgress===false?this.handleEditTransaksiBaru:null} 
+                                        />
+                                        <DeleteOutlined 
+                                            style={{ fontSize: '18px' }} 
+                                            data-id={row.id} 
+                                            onClick={isProgress===false?this.handleBtnDelete:null}
+                                        />
+                                    </TableCell>
+	                            </TableRow>
+                    		);
+                    	}):null
+                    }    
+                    </TableBody>
+                </Table>
             </TableContainer>
             <TablePagination
                 rowsPerPageOptions={[10,15,25,50,100,250,500,1000,10000]}
